c8b0996c89400d8b4552907894c54ebe5869fc42
nicoprastyo <nicoprastyo93@gmail.com>
2019-01-02 22:20:18 +0700

    configuration database and add datadummy

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('admin'),
+                'remember_token' => str_random(10),
+                'api_token' => str_random(18),
+                'is_admin' => true,
+
+        ];
+
+        DB::table('users')->insert($users);
+
+        factory(App\User::class,50)->create();
+    }
+}
