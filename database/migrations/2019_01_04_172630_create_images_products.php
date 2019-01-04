06a2c02ddb1856a2c9515604406904d3989e6534
nicoprastyo <nicoprastyo93@gmail.com>
2019-01-05 01:08:23 +0700

    Setup database

---
+++ database/migrations/2019_01_04_172630_create_images_products.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned();
+            $table->string('image',50)->nullable();
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images_products');
+    }
+}
